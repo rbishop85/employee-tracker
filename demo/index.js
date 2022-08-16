796ff63d1918f18773a50d753ba11d194198ccdd
Robert Bishop <rbishop85@gmail.com>
2022-08-16 08:31:03 -0700

    Installed npm packages

---
+++ demo/index.js
@@ -0,0 +1,20 @@
+
+
+
+
+// View all departments
+    // SELECT * FROM department;
+
+// View all roles
+    // SELECT * FROM role;
+
+// View all empoyees
+    // SELECT * FROM empoyee (try to bring in joins to mirror example video);
+
+// Create new department
+    // INSERT INTO department (name)
+        // VALUES ("Sales");
+
+// Create a new role
+    // INSERT INTO role (title, salary, department_id)
+        // VALUES ("Manager", 120000, 1)
