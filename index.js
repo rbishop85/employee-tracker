305adf385118aab0fb4c277fd4f0ab0a34b00fb3
Robert Bishop <rbishop85@gmail.com>
2022-08-16 23:11:19 -0700

    Setup initial what to do question and worked on running functions based on answers

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2');
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 
 const db = mysql.createConnection(
     {
@@ -12,11 +12,76 @@ const db = mysql.createConnection(
   );
 
 function whatToDo() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      message: "What would you like to do?",
+      name: 'choice',
+      choices: [
+        'View All Departments',
+        'Add A Department',
+        'View All Roles',
+        'Add A Role',
+        'View All Employees',
+        'Add An Employee',
+        `Update An Employee's Role`,
+      ],
+      prefix: '-'
+    }
+  ])
+  .then((response) => {
+    switch (response.choice) {
+      case 'View All Departments':
+        viewDept();
+        break;
+      case 'Add A Department':
+        addDept();
+        break;
+      case 'View All Roles':
+        viewRole();
+        break;
+      case 'Add A Role':
+        addRole();
+        break;
+      case 'View All Employees':
+        viewEmp();
+        break;
+      case 'Add An Employee':
+        addEmp();
+        break;
+      case `Update An Employee's Role`:
+        updateEmp();
+        break;
+    }
+  })
 
 };
 
 function init() {
   whatToDo();
 }
+
+const viewDept = () => {
+  console.log("Viewing Departments");
+};
+const addDept = () => {
+  console.log("Adding Department");
+};
+const viewRole = () => {
+  console.log("Viewing Roles");
+};
+const addRole = () => {
+  console.log("Adding Roles");
+};
+const viewEmp = () => {
+  console.log("Viewing Employees");
+};
+const addEmp = () => {
+  console.log("Adding Employees");
+};
+const updateEmp = () => {
+  console.log("Updating Employees");
+};
+
 
 init();
